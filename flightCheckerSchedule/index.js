be6dc606fe5a9339ffcfcfda4f0f4c25374cbef8
nathanguk <nathangaskill@hotmail.com>
2018-05-11 20:44:54 +0100

    updates

--- flightCheckerSchedule/index.js
+++ flightCheckerSchedule/index.js
@@ -3,9 +3,25 @@ module.exports = function (context, flightCheckerTimer) {
     var azure = require('azure-storage');
     var request = require('request');
 
-    // Create Table service connection./.
-    var tableSvc = azure.createTableService();
+    // Get timestamp and print time
+    var timeStamp = new Date().toISOString();
+    context.log('JavaScript timer trigger function ran!:', timeStamp);
 
+    // Create Queue service connection
+    var queueService = azure.createQueueService();
+
+    // Check if queues exist and create it if not
+    queueService.createQueueIfNotExists('flightchecker', function(error) {
+        if(!error) {
+          // Queue created or exists
+          context.log("Create queue 'flightchecker' or already exists");
+        }
+    });
+
+    // Create Table service connection
+    var tableSvc = azure.createTableService();
+    
+    // Check if tables exist and create it if not
     tableSvc.createTableIfNotExists('flightCheckerChecks', function(error) {
         if(!error) { 
           // Table created or exists
@@ -14,32 +30,48 @@ module.exports = function (context, flightCheckerTimer) {
     });
 
 
+    // Check for queys to run.
     checkQuery(function (error, result){
         if(!error){
-            context.log("Result")
-            context.log(JSON.stringify(result));
-
             var checks = result.entries;   
             checks.forEach(function(check) { 
-                context.log(check.RowKey._);
+                context.log("RowKey: " + check.RowKey._);
+                var message = {
+                    "departureAirport": check.departureAirport._ ,
+                    "arrivalAirport": check.arrivalAirport._ ,
+                    "departureDate": check.departureDate._ ,
+                    "arrivalDate": check.arrivalDate._
+                };
+                checkcreateMessage(message, function (error, result){
+                    if(!error){
+                        context.log("Creted Message");
+                        context.done();
+                    }else{
+                        context.log("Error: " + error);
+                        context.done();
+                    };
+                });
             });
-
-            context.done();
         }else{
             context.log("Error: " + error);
             context.done();
         };
     });
 
-    //var timeStamp = new Date().toISOString();
-    
-    //if(flightCheckerTimer.isPastDue)
-    //{
-    //    context.log('JavaScript is running late!');
-    //}
-    //context.log('JavaScript timer trigger function ran!:', timeStamp);   
-    
-    //context.done();
+
+    //Create a new message and put in queue
+    function createMessage(message, callback) {
+        queueService.createMessage('flightchecker', message, function(error) {
+            if(!error) {
+                //context.log("Queue message result: " + result);
+                callback(null, result);
+            }else{
+                // Call the callback and pass in the error
+                //context.log("Queue message error: " + error);
+                callback(error, null);
+            }
+        });
+    };
 
 
 
@@ -53,8 +85,7 @@ module.exports = function (context, flightCheckerTimer) {
             if(!error){
                 //context.log("Query checks table result: " + result);
                 callback(null, result);
-            }
-            else{
+            }else{
                 // Call the callback and pass in the error
                 //context.log("Query checks table error: " + error);
                 callback(error, null);
